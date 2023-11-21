064b1c397df4ba170a688796f4e13fe8ad4e6366
ENG: Radwa Khaled <131785821+Radwa-Khaled@users.noreply.github.com>
2023-11-22 00:46:38 +0200

    Create 1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    # check if idx is negative or out of range.
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
