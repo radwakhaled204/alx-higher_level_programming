418e616fbb3c3b1f524fd6debcf68fe4136641f1
ENG: Radwa Khaled <131785821+Radwa-Khaled@users.noreply.github.com>
2023-11-22 01:17:08 +0200

    Create 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module defines a class Square"""
+
+
+class Square:
+    """This class represents a square with private instance attribute: size"""
+
+    def __init__(self, size=0):
+        """Initializes the square with the given size"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
