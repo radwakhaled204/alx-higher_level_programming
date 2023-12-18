c04eff160cc4f2d7e0419822674eb51fc897ad48
Radwa Khaled <131785821+Radwa-Khaled@users.noreply.github.com>
2023-12-18 22:22:21 +0200

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// This script prints 3 lines by using an array of string and a loop
+// Declare an array of strings
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+// Loop through the array
+for (let i = 0; i < lines.length; i++) {
+  // Print each element of the array
+  console.log(lines[i]);
+}
