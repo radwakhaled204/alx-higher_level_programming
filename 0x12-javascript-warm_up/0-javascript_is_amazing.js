b8fc31282b8badacde2a48114ac7613f6389b129
Radwa Khaled <131785821+Radwa-Khaled@users.noreply.github.com>
2023-12-18 22:10:57 +0200

    0-javascript_is_amazing.js

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// This script prints "JavaScript is amazing"
+// Declare a constant variable with value
+const myVar = 'JavaScript is amazing';
+// Print the value of the variable to the standard output
+console.log(myVar);
