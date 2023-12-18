529baed7c5eb758d0d4f77d0d7593b806a513cb6
Radwa Khaled <131785821+Radwa-Khaled@users.noreply.github.com>
2023-12-18 19:26:48 +0200

    2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This a script that checks if if the object
+is exactly an instance of the specified class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Returns True if the object is exactly an instance of the specified class
+    """
+
+    return type(obj) is a_class
