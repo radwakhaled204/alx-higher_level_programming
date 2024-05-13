1b95bfe53d2dbda228bfc0b4155a387c2614e5e2
Radwa Khaled <131785821+radwakhaled204@users.noreply.github.com>
2024-05-13 04:22:08 +0200

    9-script.js

---
+++ 0x15-javascript-web_jquery /9-script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $.ajax({
+    url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
+    success: function (data) {
+      $('#hello').text(data.hello);
+    }
+  });
+});
